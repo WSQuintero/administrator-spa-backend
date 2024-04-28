b738bc67d288c1cf5922484f7e30191aff076dfa
Santiago Quintero <santiago@conexioncibernetica.com>
2024-04-27 19:58:16 -0500

    fix extensions

--- servers/server-with-firebase.js
+++ servers/server-with-firebase.js
@@ -1,10 +1,10 @@
 import { createApp } from "../app.js"
-import { DatesModelFirebase } from "../models/datesModelFireBase.js"
-import { BillsModelFirebase } from "../models/billsModelFirebase.js"
-import { SalesModelFirebase } from "../models/salesModelFirebase.js"
-import { LoginModelFirebase } from "../models/loginModelFirebase.js"
-import { SignUpModelFirebase } from "../models/signUpModelFirebase.js"
-import { StatisticsModelFirebase } from "../models/statisticsModelFirebase.js"
+import { DatesModelFirebase } from "./../models/datesModelFireBase.js"
+import { BillsModelFirebase } from "./../models/billsModelFirebase.js"
+import { SalesModelFirebase } from "./../models/salesModelFirebase.js"
+import { LoginModelFirebase } from "./../models/loginModelFirebase.js"
+import { SignUpModelFirebase } from "./../models/signUpModelFirebase.js"
+import { StatisticsModelFirebase } from "./../models/statisticsModelFirebase.js"
 
 createApp({
   DatesModel: DatesModelFirebase,
